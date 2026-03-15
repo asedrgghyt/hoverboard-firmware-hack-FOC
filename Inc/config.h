23a6bb75d593f71cff3a2a204d4c88970f25d3c1
asedrgghyt <155243148+asedrgghyt@users.noreply.github.com>
2026-03-15 13:44:35 +0000

    saegaratasv1.2

--- Inc/config.h
+++ Inc/config.h
@@ -526,24 +526,24 @@
   // #define ELECTRIC_BRAKE_THRES  120         // (0, 500) Threshold below at which the electric brake starts engaging.
 
   //#define MULTI_MODE_DRIVE                  // This option enables the selection of 3 driving modes at start-up using combinations of Brake and Throttle pedals (see below)
-  //#ifdef MULTI_MODE_DRIVE
+  #ifdef MULTI_MODE_DRIVE
       // BEGINNER MODE:     Power ON + Brake [released] + Throttle [released or pressed]
-      //#define MULTI_MODE_DRIVE_M1_MAX   200
-      //#define MULTI_MODE_DRIVE_M1_RATE  300
-      //#define MULTI_MODE_M1_I_MOT_MAX   8
-      //#define MULTI_MODE_M1_N_MOT_MAX   100
+      #define MULTI_MODE_DRIVE_M1_MAX   200
+      #define MULTI_MODE_DRIVE_M1_RATE  300
+      #define MULTI_MODE_M1_I_MOT_MAX   8
+      #define MULTI_MODE_M1_N_MOT_MAX   100
 
       // INTERMEDIATE MODE: Power ON + Brake [pressed] + Throttle [released]
-      //#define MULTI_MODE_DRIVE_M2_MAX   500
-      //#define MULTI_MODE_DRIVE_M2_RATE  300
-      //#define MULTI_MODE_M2_I_MOT_MAX   8
-      //#define MULTI_MODE_M2_N_MOT_MAX   200
+      #define MULTI_MODE_DRIVE_M2_MAX   500
+      #define MULTI_MODE_DRIVE_M2_RATE  300
+      #define MULTI_MODE_M2_I_MOT_MAX   8
+      #define MULTI_MODE_M2_N_MOT_MAX   200
 
       // ADVANCED MODE:    Power ON + Brake [pressed] + Throttle [pressed]
-      //#define MULTI_MODE_DRIVE_M3_MAX   2500
-      //#define MULTI_MODE_DRIVE_M3_RATE  480
-      //#define MULTI_MODE_M3_I_MOT_MAX   15
-      //#define MULTI_MODE_M3_N_MOT_MAX   4000
+      #define MULTI_MODE_DRIVE_M3_MAX   2500
+      #define MULTI_MODE_DRIVE_M3_RATE  480
+      #define MULTI_MODE_M3_I_MOT_MAX   15
+      #define MULTI_MODE_M3_N_MOT_MAX   4000
   #endif
 
 #endif
