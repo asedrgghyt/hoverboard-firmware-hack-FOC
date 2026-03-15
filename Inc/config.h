b6da38bd2d89ee968aa25e67b511532b54644e48
asedrgghyt <155243148+asedrgghyt@users.noreply.github.com>
2026-03-15 15:39:41 +0000

    saega ratas1.10

--- Inc/config.h
+++ Inc/config.h
@@ -521,8 +521,8 @@
   // Extra functionality
   // #define CRUISE_CONTROL_SUPPORT            // [-] Flag to enable Cruise Control support. Activation/Deactivation is done by sideboard button or Brake pedal press.
   // #define STANDSTILL_HOLD_ENABLE            // [-] Flag to hold the position when standtill is reached. Only available and makes sense for VOLTAGE or TORQUE mode.
-  // #define ELECTRIC_BRAKE_ENABLE             // [-] Flag to enable electric brake and replace the motor "freewheel" with a constant braking when the input torque request is 0. Only available and makes sense for TORQUE mode.
-  #define ELECTRIC_BRAKE_MAX    100         // (0, 500) Maximum electric brake to be applied when input torque request is 0 (pedal fully released).
+  #define ELECTRIC_BRAKE_ENABLE             // [-] Flag to enable electric brake and replace the motor "freewheel" with a constant braking when the input torque request is 0. Only available and makes sense for TORQUE mode.
+  #define ELECTRIC_BRAKE_MAX    20         // (0, 500) Maximum electric brake to be applied when input torque request is 0 (pedal fully released).
   // #define ELECTRIC_BRAKE_THRES  120         // (0, 500) Threshold below at which the electric brake starts engaging.
 
   #define MULTI_MODE_DRIVE                  // This option enables the selection of 3 driving modes at start-up using combinations of Brake and Throttle pedals (see below)
@@ -531,19 +531,19 @@
       #define MULTI_MODE_DRIVE_M1_MAX   2500
       #define MULTI_MODE_DRIVE_M1_RATE  480
       #define MULTI_MODE_M1_I_MOT_MAX   15
-      #define MULTI_MODE_M1_N_MOT_MAX   2000
+      #define MULTI_MODE_M1_N_MOT_MAX   3000
 
       // INTERMEDIATE MODE: Power ON + Brake [pressed] + Throttle [released]
       #define MULTI_MODE_DRIVE_M2_MAX   2500
       #define MULTI_MODE_DRIVE_M2_RATE  480
       #define MULTI_MODE_M2_I_MOT_MAX   15
-      #define MULTI_MODE_M2_N_MOT_MAX   2000
+      #define MULTI_MODE_M2_N_MOT_MAX   3000
 
       // ADVANCED MODE:    Power ON + Brake [pressed] + Throttle [pressed]
       #define MULTI_MODE_DRIVE_M3_MAX   2500
       #define MULTI_MODE_DRIVE_M3_RATE  480
       #define MULTI_MODE_M3_I_MOT_MAX   15
-      #define MULTI_MODE_M3_N_MOT_MAX   2000
+      #define MULTI_MODE_M3_N_MOT_MAX   3000
   #endif
 
 #endif
